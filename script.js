7c32ac1e84503dc1a22f758e6235a9b62e625409
Ique <iquecode@gmail.com>
2021-06-21 01:30:10 -0300

    criacao logica geracao combinacoes ok

--- script.js
+++ script.js
@@ -3,12 +3,19 @@ let broadModified = true;
 let phrase = true;
 let exact = true;
 
+const textAreaKeyWords = new Object();
+textAreaKeyWords.A = document.getElementById('iquedev-word-key-A');
+textAreaKeyWords.B = document.getElementById('iquedev-word-key-B');
+textAreaKeyWords.C = document.getElementById('iquedev-word-key-C');
+textAreaKeyWords.D = document.getElementById('iquedev-word-key-D');
+textAreaKeyWords.E = document.getElementById('iquedev-word-key-E');
 
 
 function iquedevControlChoiceCorrespondence(type)
 {
 
-    console.log("CLICOU!");
+    //console.log("CLICOU!");
+    generateKeyWords();
 
     const prefix       = 'iquedev-btn-correspondence-';
     const classDisable = prefix + 'disabled';
@@ -127,5 +134,266 @@ function checkBtnCombinationPossibilites()
     // combinationBtnsActive.forEach(element => {
     //     console.log(element);
     // });
+}
+
+
+function generateKeyWords()
+{
+    const activeBtns = document.getElementsByClassName('iquedev-btn-combination-active');
+
+    generateCombinatios = [];
+    for (let i = 0; i < activeBtns.length; i++)
+    {
+        // generateCombinatios é alimentado em cada posição com arrei das combinações de cada botão ativo
+        generateCombinatios.push( activeBtns[i].id.substr(24).split('+')  );
+        // activeBtns[i].id.substr(24).split('+').forEach(letter => {
+        // });
+    }
+
+    let list = [];
+    generateCombinatios.forEach(combination => {
+        
+        let words = [];
+        combination.forEach(letter => {
+            //const array = textAreaKeyWords[letter].value.split(',');
+            const array_words = textAreaKeyWords[letter].value.split(',').map( function(item) {
+                return item.trim(); 
+            }); 
+
+            words.push( array_words );
+
+            // const words = textAreaKeyWords[letter].value;
+            // console.log(words);
+        });
+
+        // aqui temos o array de arrays words de cada botão habilitado.... combinar cada as palavras de cada posição co
+        //console.log(words);
+        list = [...list, ...generateCombinatiosEngine(words)];
+    });
+    console.log ( list );
+    fillResultsTextArea(list);
+
+    //console.log(generateCombinatios);
 
 }
+
+
+function teste()
+{
+
+     words = [];
+     words[0] = ['praia', 'campo', 'cidade'];
+     words[1] = ['sol', 'chuva'];
+
+    list = [];
+    for (let x = 0; x < words[0].length; x++)
+    {
+        for (let y = 0; y < words[1].length; y++ )
+        {	
+            
+            list.push ( words[0][x] + ' ' + words[1][y]  );							
+            
+        }
+    }
+    console.log( list );
+
+}
+
+
+function teste2()
+{
+
+     words = [];
+     words[0] = ['praia', 'campo', 'cidade'];
+     words[1] = ['sol', 'chuva'];
+     words[2] = ['quente', 'frio', 'ameno'];
+
+    list = [];
+    for (let x = 0; x < words[0].length; x++)
+    {
+        for (let y = 0; y < words[1].length; y++ )
+        {	
+            for (let z = 0; z < words[2].length; z++) 
+            {
+                list.push ( words[0][x] + ' ' + words[1][y] + ' ' + words[2][z]  );							
+            }	
+        }
+    }
+    console.log( list );
+
+}
+
+function teste3()
+{
+     words = [];
+     words[0] = ['praia', 'campo', 'cidade'];
+     words[1] = ['sol', 'chuva'];
+     words[2] = ['quente', 'frio', 'ameno'];
+     words[3] = ['chato', 'legal'];
+
+    list = [];
+    for (let x = 0; x < words[0].length; x++)
+    {
+        for (let y = 0; y < words[1].length; y++ )
+        {	
+            for (let z = 0; z < words[2].length; z++) 
+            {
+                for (let za = 0; za < words[3].length; za++) 
+                {
+                    list.push ( words[0][x] + ' ' + words[1][y] + ' ' + words[2][z] + ' ' + words[3][za]);		
+                }				
+            }	
+        }
+    }
+    console.log( list );
+}
+
+
+function teste4()
+{
+     words = [];
+     words[0] = ['praia', 'campo', 'cidade'];
+     words[1] = ['sol', 'chuva'];
+     words[2] = ['quente', 'frio', 'ameno'];
+     words[3] = ['chato', 'legal'];
+     words[4] = ['gambiarra', 'funciona'];
+
+    list = [];
+    for (let x = 0; x < words[0].length; x++)
+    {
+        for (let y = 0; y < words[1].length; y++ )
+        {	
+            for (let z = 0; z < words[2].length; z++) 
+            {
+                for (let za = 0; za < words[3].length; za++) 
+                {
+                    for (let zb = 0; zb < words[4].length; zb++) 
+                    {
+                        list.push ( words[0][x] + ' ' + words[1][y] + ' ' + words[2][z] + ' ' + words[3][za] + ' ' + words[4][zb]);
+                    }
+                }				
+            }	
+        }
+    }
+    console.log( list );
+}
+
+
+
+
+
+
+
+
+function generateCombinatiosEngine(words)
+{
+
+
+    list = [];
+
+    switch ( words.length ) {
+        case 2:
+            for (let x = 0; x < words[0].length; x++)
+            {
+                for (let y = 0; y < words[1].length; y++ )
+                {	
+                    list.push ( words[0][x] + ' ' + words[1][y]  );							
+                }
+            }
+            break;
+
+        case 3:
+            for (let x = 0; x < words[0].length; x++)
+            {
+                for (let y = 0; y < words[1].length; y++ )
+                {	
+                    for (let z = 0; z < words[2].length; z++) 
+                    {
+                        list.push ( words[0][x] + ' ' + words[1][y] + ' ' + words[2][z]  );							
+                    }	
+                }
+            }
+            break;
+
+        case 4:
+            for (let x = 0; x < words[0].length; x++)
+            {
+                for (let y = 0; y < words[1].length; y++ )
+                {	
+                    for (let z = 0; z < words[2].length; z++) 
+                    {
+                        for (let za = 0; za < words[3].length; za++) 
+                        {
+                            list.push ( words[0][x] + ' ' + words[1][y] + ' ' + words[2][z] + ' ' + words[3][za]);		
+                        }				
+                    }	
+                }
+            }
+            break;
+    
+        case 5:
+            for (let x = 0; x < words[0].length; x++)
+            {
+                for (let y = 0; y < words[1].length; y++ )
+                {	
+                    for (let z = 0; z < words[2].length; z++) 
+                    {
+                        for (let za = 0; za < words[3].length; za++) 
+                        {
+                            for (let zb = 0; zb < words[4].length; zb++) 
+                            {
+                                list.push ( words[0][x] + ' ' + words[1][y] + ' ' + words[2][z] + ' ' + words[3][za] + ' ' + words[4][zb]);
+                            }
+                        }				
+                    }	
+                }
+            }
+            break;
+
+        default:
+            return false;
+    }
+
+    return list;
+ 
+
+}
+
+
+
+
+function fillResultsTextArea(list)
+{
+
+    const textArea = document.getElementById('iquedev-results-text-area');
+
+    let content = '';
+    list.forEach(item => {
+        content = content + item + '\r\n';
+    });
+
+    textArea.value = content;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//teste3();
